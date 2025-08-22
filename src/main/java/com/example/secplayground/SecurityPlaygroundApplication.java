5e2857fb08cc144ac3ccacfa5933340341853463
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-22 18:03:36 +0100

    init project

---
+++ src/main/java/com/example/secplayground/SecurityPlaygroundApplication.java
@@ -0,0 +1,11 @@
+package com.example.secplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPlaygroundApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityPlaygroundApplication.class, args);
+    }
+}
