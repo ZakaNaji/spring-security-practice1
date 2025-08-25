862165c0f47fbd045b4b662be5ed068dd4fbb8dd
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-25 12:40:27 +0100

    add customer and authority entities

---
+++ src/main/java/com/example/secplayground/model/Authority.java
@@ -0,0 +1,27 @@
+package com.example.secplayground.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<Customer> customers = new HashSet<>();
+
+}
---
+++ src/main/java/com/example/secplayground/model/Customer.java
@@ -0,0 +1,35 @@
+package com.example.secplayground.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table
+@Setter @Getter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private boolean enabled = true;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+
+    @ManyToMany()
+    @JoinTable(
+            name = "customer_authority",
+            joinColumns = @JoinColumn(name = "customer_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities = new HashSet<>();
+}
