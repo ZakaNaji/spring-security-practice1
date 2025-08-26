61fe95f788cbf76fd21b3efed65671a21350e8c9
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-26 17:49:47 +0100

    exceptions to throw in case usenrname/email was already used

---
+++ src/main/java/com/example/secplayground/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.example.secplayground.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+}
---
+++ src/main/java/com/example/secplayground/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.example.secplayground.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+}
