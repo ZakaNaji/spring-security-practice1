f23223239a7aba3a4304360a895fae279470bb1f
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-28 11:10:47 +0100

    find token repos method

--- src/main/java/com/example/secplayground/model/VerificationTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.secplayground.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-}
